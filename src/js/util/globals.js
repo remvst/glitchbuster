40fd72c802923bcbbd2bd4ed873fe2c3acbf2f60
remvst <remvst@gmail.com>
2016-08-13 20:52:28 -0400

    Tiny optimization

--- src/js/util/globals.js
+++ src/js/util/globals.js
@@ -7,23 +7,20 @@ var W = window,
     P, // player
     V, // camera,
     K = {}, // keyboard
-    noop = function(){}
-    ;
+    noop = function(){},
+    raf = (function(){
+        return  W.requestAnimationFrame       ||
+                W.webkitRequestAnimationFrame ||
+                W.mozRequestAnimationFrame    ||
+                function(c){
+                    T(c, 1000 / 60);
+                };
+    })(),
+    toBool = function(x){
+        return !!x;
+    };
 
 // Exposing all math functions to the global scope
 Object.getOwnPropertyNames(Math).forEach(function(n){
     W[n] = M[n];
 });
-
-var raf = (function(){
-    return  W.requestAnimationFrame       ||
-            W.webkitRequestAnimationFrame ||
-            W.mozRequestAnimationFrame    ||
-            function(c){
-                T(c, 1000 / 60);
-            };
-})();
-
-var toBool = function(x){
-    return !!x;
-};
