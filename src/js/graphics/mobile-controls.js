59f253c213886fd5ab4644fee9f4a76a52100eaa
remvst <remvst@gmail.com>
2016-08-21 21:12:25 -0400

    Tabs are evil

--- src/js/graphics/mobile-controls.js
+++ src/js/graphics/mobile-controls.js
@@ -1,35 +1,35 @@
 var
     rightArrow = cache(MOBILE_BUTTON_SIZE, MOBILE_BUTTON_SIZE, function(c, r){
         with(r){
-        	fillStyle = '#fff';
-        	beginPath();
-        	moveTo(0, 0);
-        	lineTo(MOBILE_BUTTON_SIZE, MOBILE_BUTTON_SIZE / 2);
-        	lineTo(0, MOBILE_BUTTON_SIZE);
-        	fill();
+            fillStyle = '#fff';
+            beginPath();
+            moveTo(0, 0);
+            lineTo(MOBILE_BUTTON_SIZE, MOBILE_BUTTON_SIZE / 2);
+            lineTo(0, MOBILE_BUTTON_SIZE);
+            fill();
         }
     }),
     leftArrow = cache(MOBILE_BUTTON_SIZE, MOBILE_BUTTON_SIZE, function(c, r){
         with(r){
-        	translate(MOBILE_BUTTON_SIZE, 0);
-        	scale(-1, 1);
-        	drawImage(rightArrow, 0, 0);
+            translate(MOBILE_BUTTON_SIZE, 0);
+            scale(-1, 1);
+            drawImage(rightArrow, 0, 0);
         }
     }),
     jumpArrow = cache(MOBILE_BUTTON_SIZE, MOBILE_BUTTON_SIZE, function(c, r){
         with(r){
-        	translate(0, MOBILE_BUTTON_SIZE);
-        	rotate(-PI / 2);
+            translate(0, MOBILE_BUTTON_SIZE);
+            rotate(-PI / 2);
 
-        	drawImage(rightArrow, 0, 0);
+            drawImage(rightArrow, 0, 0);
         }
     }),
     grenadeButton = cache(MOBILE_BUTTON_SIZE, MOBILE_BUTTON_SIZE, function(c, r){
         with(r){
-        	fillStyle = '#fff';
-        	beginPath();
-        	arc(MOBILE_BUTTON_SIZE / 2, MOBILE_BUTTON_SIZE / 2, MOBILE_BUTTON_SIZE / 2, 0, PI * 2, true);
-        	fill();
+            fillStyle = '#fff';
+            beginPath();
+            arc(MOBILE_BUTTON_SIZE / 2, MOBILE_BUTTON_SIZE / 2, MOBILE_BUTTON_SIZE / 2, 0, PI * 2, true);
+            fill();
         }
     })
     ;
