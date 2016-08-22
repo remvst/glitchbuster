e7aa9a42ed787cec2dc166d7a4036cb5310e0ad5
remvst <remvst@gmail.com>
2016-08-21 21:31:07 -0400

    Should save some elses

--- src/js/util/interp.js
+++ src/js/util/interp.js
@@ -14,13 +14,14 @@ function oscillate(t, b, c, d) {
 function easeOutBounce(t, b, c, d) {
     if ((t/=d) < (1/2.75)) {
         return c*(7.5625*t*t) + b;
-    } else if (t < (2/2.75)) {
-        return c*(7.5625*(t-=(1.5/2.75))*t + .75) + b;
-    } else if (t < (2.5/2.75)) {
-        return c*(7.5625*(t-=(2.25/2.75))*t + .9375) + b;
-    } else {
-        return c*(7.5625*(t-=(2.625/2.75))*t + .984375) + b;
     }
+    if (t < (2/2.75)) {
+        return c*(7.5625*(t-=(1.5/2.75))*t + .75) + b;
+    }
+    if (t < (2.5/2.75)) {
+        return c*(7.5625*(t-=(2.25/2.75))*t + .9375) + b;
+    }
+    return c*(7.5625*(t-=(2.625/2.75))*t + .984375) + b;
 }
 
 function interp(o, p, a, b, d, l, f, e){
