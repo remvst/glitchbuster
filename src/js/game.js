53d8eb7c7387c0c60a3168e98aa71866350cdf6e
remvst <remvst@gmail.com>
2016-09-01 22:03:31 -0400

    Fixed enemies in tutorial

--- src/js/game.js
+++ src/js/game.js
@@ -66,15 +66,15 @@ function Game(){
         if(!G.currentLevel){
             // Put the enemies at the right spots
             var enemy1 = new WalkingEnemy();
-            enemy1.x = 5400;
-            enemy1.y = 1000;
+            enemy1.x = 4500;
+            enemy1.y = 800;
             G.cyclables.push(enemy1);
             G.killables.push(enemy1);
             G.renderables.push(enemy1);
 
             var enemy2 = new JumpingEnemy();
-            enemy2.x = 7000;
-            enemy2.y = 1000;
+            enemy2.x = 5700;
+            enemy2.y = 800;
             G.cyclables.push(enemy2);
             G.killables.push(enemy2);
             G.renderables.push(enemy2);
