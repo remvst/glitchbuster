01051e3079fec4e6292c3187f603debb131779f8
remvst <remvst@gmail.com>
2016-09-07 16:28:39 -0700

    Fixed tweet

--- src/js/menu/game-over.js
+++ src/js/menu/game-over.js
@@ -14,7 +14,7 @@ function GameOverMenu(reason){
     this.button(button(nomangle('share')), 0, 700, function(){
         open(nomangle('//twitter.com/intent/tweet?') +
             nomangle('hashtags=js13k') +
-            nomangle('&url=') + encodeURIComponent(nomangle('//goo.gl/TIwjsW')) +
+            nomangle('&url=') + encodeURIComponent(nomangle('http://js13kgames.com/entries/glitchbuster')) +
             nomangle('&text=') + encodeURIComponent(
                 (reason == GAME_OVER_SUCCESS ? nomangle('I fixed all glitches in ') + t : nomangle('I fixed ') + (G.currentLevel - 1) + nomangle('/13 glitches')) + nomangle(' on Glitchbuster!')
             )
@@ -28,11 +28,7 @@ function GameOverMenu(reason){
 
     this.animateButtons();
 
-    if(reason == GAME_OVER_SUCCESS){
-        ss.push(nomangle('time: ') + t);
-    }else{
-        ss.push(nomangle('fixed ') + (G.currentLevel - 1) + '/13');
-    }
+    ss.push(reason == GAME_OVER_SUCCESS ? nomangle('time: ') + t : nomangle('fixed ') + (G.currentLevel - 1) + '/13');
 
     var s1 = ss[0],
         t1 = 10,
