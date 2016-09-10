2ca47cf39209a1544b0f729b5b999cb8df51c58f
remvst <remvst@gmail.com>
2016-09-10 13:22:57 -0700

    Gave button() its own file

--- src/js/graphics/font.js
+++ src/js/graphics/font.js
@@ -382,25 +382,3 @@ function cachedCharacter(t, s, c){
     }
     return cachedChars[key];
 }
-
-function button(t, w){
-    w = w || 440;
-    return cache(w, 100, function(r){
-        with(r){
-            fillStyle = '#444';
-            fillRect(0, 90, w, 10);
-
-            fillStyle = '#fff';
-            fillRect(0, 0, w, 90);
-
-            drawText(r, '::' + t + '()', 100, 20, 10, '#000');
-
-            fillStyle = '#000';
-            beginPath();
-            moveTo(40, 20);
-            lineTo(80, 45);
-            lineTo(40, 70);
-            fill();
-        }
-    });
-}
