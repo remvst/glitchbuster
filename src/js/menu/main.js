819f60bca78fa0980dcfc09d22e8c5bc21127bdc
remvst <remvst@gmail.com>
2016-09-10 22:58:15 -0700

    Useless stuff

--- src/js/menu/main.js
+++ src/js/menu/main.js
@@ -17,12 +17,15 @@ function MainMenu(){
 
     var titleX = (CANVAS_WIDTH - 460) / 2;
     this.button(cache(460, 230, function(r){
-    	drawText(r, 'glitch', 0, 10, 20, '#444');
-    	drawText(r, 'glitch', 0, 0, 20, '#fff');
+        drawText(r, 'glitch', 0, 10, 20, '#444');
+        drawText(r, 'glitch', 0, 0, 20, '#fff');
 
-    	drawText(r, 'buster', 0, 130, 20, '#444');
-    	drawText(r, 'buster', 0, 120, 20, '#fff');
-    }), titleX, 90);
+        drawText(r, 'buster', 0, 130, 20, '#444');
+        drawText(r, 'buster', 0, 120, 20, '#fff');
+    }), titleX, 90, function(){
+        // Got spare bites, so yolo
+        interp(this, 'o', 0, 1, 0.4);
+    });
 
     interp(this.buttons[this.buttons.length - 1], 'o', 0, 1, 0.25, 0.5);
 }
